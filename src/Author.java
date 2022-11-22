312d025cc59a00b8179ee7a02d035a09d3b608b2
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-11-22 13:17:56 -0500

    better_fix exercises

--- src/Author.java
+++ src/Author.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
-    /**
-     * @deprecated Use publishedBooks instead
-     */
-    @Deprecated
+//    /**
+//     * @deprecated Use publishedBooks instead
+//     */
+//    @Deprecated
     public List<String> getBooks() {
         return books;
     }
